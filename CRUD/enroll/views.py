b6abba0fd87d94e4aadd918fe00a77d79b21c845
Talhak <talhawkk@gmail.com>
2025-01-11 20:40:18 +0500

    create table in addshow template to show all saved data

--- CRUD/enroll/views.py
+++ CRUD/enroll/views.py
@@ -6,12 +6,13 @@ def show(request):
     if request.method=='POST':
         fm=StudentForm(request.POST)
         if fm.is_valid():
-            print(fm.cleaned_data)
-            nm=fm.cleaned_data['name']
-            em=fm.cleaned_data['email']
-            pw=fm.cleaned_data['password']
-            st=Student(name=nm,email=em,password=pw)
-            st.save()
+            # print(fm.cleaned_data)
+            # nm=fm.cleaned_data['name']
+            # em=fm.cleaned_data['email']
+            # pw=fm.cleaned_data['password']
+            # st=Student(name=nm,email=em,password=pw)
+            # st.save()
+            fm.save()
             # return render(request,'enroll/home.html',{'form':fm.cleaned_data})
     else:
         fm=StudentForm()
@@ -22,16 +23,19 @@ def addshow(request):
     if request.method=='POST':
         fm=StudentForm(request.POST)
         if fm.is_valid():
-            print(fm.cleaned_data)
-            nm=fm.cleaned_data['name']
-            em=fm.cleaned_data['email']
-            pw=fm.cleaned_data['password']
-            st=Student(name=nm,email=em,password=pw)
-            st.save()
+            # print(fm.cleaned_data)
+            # nm=fm.cleaned_data['name']
+            # em=fm.cleaned_data['email']
+            # pw=fm.cleaned_data['password']
+            # st=Student(name=nm,email=em,password=pw)
+            fm.save()
+            fm=StudentForm()
+
                 # return render(request,'enroll/home.html',{'form':fm.cleaned_data})
     else:
+            stu=Student.objects.all()
             fm=StudentForm()
-    return render(request,'enroll/addshow.html',{'form':fm})
+    return render(request,'enroll/addshow.html',{'form':fm,'stud':stu})
 
 
 def update(request):
